2c6034692f980f6cf98a84be8c490d1c92319848
LiteBunny <blakewinfred@gmail.com>
2023-03-22 22:15:08 +1300

    Debug

--- DHCP-Server-Config.sh
+++ DHCP-Server-Config.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
-#check the user
-if [ $EUID -ne 0 ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-#get system information
-cat /proc/version |read sys_info
-VerifyString=debian
-
-#Identify System
-if [[$sys_info =~ $VerifyString ]];then
-	echo "instaling packages"
-else
-	echo "Sorry This script only work on Debian" 1>&2 && exit 1
-fi
-
-if [ ! -d "/etc/default/isc-dhcp-server" ]; then #verify if dhcp server has been installed
-   installDhcpServer
-   expectGenerate
-   configGenerate
-   changeInterface
-else
-echo "isc-dhcp-server installed skip install"
-   expectGenerate
-   configGenerate
-   changeInterface
-fi
-
-
 
 # Functions Sections
 function installDhcpServer() {
@@ -48,19 +18,19 @@ echo "Config files"
 echo "Folliw the guide to complete the config"
 echo "====================="
 
-echo "Which interface(Port) you want to provie service,one interface only\n"
-read interfac
+echo -e "Which interface(Port) you want to provie service,one interface only eg.eth0\n"
+read interface
 
 echo "import subnet range eg.192.168.1.0:"
 read subNet
-echo "import net mask:"
+echo "import net mask eg.255.255.255.0:"
 read netMask
 echo "Import Router:"
 read Router
 echo "Import DNS server address:"
 read nameServer
-read -p "Import start range:" startRange
-read -p "Import end range:" endRange
+read -p "Import start range eg.192.168.1.100:" startRange
+read -p "Import end range eg.192.168.1.200:" endRange
 read -p "Import domain search :" domainSearch
 }
 
@@ -70,7 +40,7 @@ example Config
 subnet 192.168.10.0 netmask 255.255.255.0 {
 option routers 192.168.10.1;
 option subnet-mask 255.255.255.0;
-option domain-search “tecmint.lan”;
+option domain-search "tecmint.lan";
 option domain-name-servers 192.168.10.1;
 range 192.168.10.10 192.168.10.100;
 range 192.168.10.110 192.168.10.200;
@@ -79,30 +49,82 @@ BLOCK
 
 function configGenerate(){
 echo   "subnet $subNet netmask $netMask {" >> /tmp/DHCP-exp
-echo   "option routers $Router;" >> /tmp/DHCP-exp
-echo   "option subnet-mask $netMask;" >> /tmp/DHCP-exp
-echo   "option domain-search “$domainSearch”;" >> /tmp/DHCP-exp
-echo   "option domain-name-servers $nameServer;" >> /tmp/DHCP-exp
-echo   "range $startRange $endRange;" >> /tmp/DHCP-exp
-echo "Config overview\n"
+echo   " option routers $Router;" >> /tmp/DHCP-exp
+echo   " option subnet-mask $netMask;" >> /tmp/DHCP-exp
+echo   " option domain-search \"$domainSearch\";" >> /tmp/DHCP-exp
+echo   " option domain-name-servers $nameServer;" >> /tmp/DHCP-exp
+echo   " range $startRange $endRange;" >> /tmp/DHCP-exp
+echo   "}" >> /tmp/DHCP-exp
+echo -e "Config overview\n"
+echo -e "==================\n"
 cat /tmp/DHCP-exp
+echo -e "==================\n"
+echo "You may need to manualy change the default settings in config file"
+echo "=============="
 read -p "Do you want to apply this config[Y/n]" ApplyStatus
-if [$ApplyStatus -ne n];then
-   cat /tmp/DHCP-exp >> /etc/default/isc-dhcp-server
-else
+case $ApplyStatus in
+   [yY])
+      cat /tmp/DHCP-exp >> /etc/dhcp/dhcpd.conf
+      rm /tmp/DHCP-exp;;
+   *)
    echo "Not apply"
-fi
+   rm /tmp/DHCP-exp;;
+esac
 }
 
 
 function changeInterface(){
    echo "currect config"
-   grep "INTERFACES" /etc/default/isc-dhcp-server
+   echo $(grep "INTERFACES" /etc/default/isc-dhcp-server)
    read -p "DO you want to change it[y/N]" intfChoice
-   if [$intfChoice -ne y ];then
-   echo "No changes"
-   else
-   sed -i "s/^.*INTERFACESv4.*$/INTERFACESv4=“$interface”/" /etc/default/isc-dhcp-server
-   echo "Complet"
-   fi   
+   case $intfChoice in
+      [yY]* )
+      sed -i "s/^.*INTERFACESv4.*$/INTERFACESv4=\"$interface\"/" /etc/default/isc-dhcp-server
+      sed -i "s/^.*INTERFACESv6.*$/INTERFACESv6=\"$interface\"/" /etc/default/isc-dhcp-server
+      echo "Complet";;
+      * )
+      echo "No changes"
+   esac 
 }
+
+# Program Start
+#check the user ID
+if [ $EUID -ne 0 ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+#get system information
+sys_info=$(cat /proc/version|grep debian)
+VerifyString=debian
+
+#Identify System
+if [[ $sys_info =~ $VerifyString ]];then
+	echo "instaling packages"
+else
+	echo "Sorry This script only work on Debian" 1>&2 && exit 1
+fi
+
+
+
+BeforeInstallWorning=understand
+echo "Before you run this, make SHURE you have configed a static IP address On this Server"
+read -p "please retype [$BeforeInstallWorning]" InstallWorning
+if [$BeforeInstallWorning -ne $InstallWorning];then
+   echo "Please maky shure you have a static IP" 1>&2
+   exit 1
+fi
+
+# Auto detact is the server is installed add process
+if [ ! -f "/etc/default/isc-dhcp-server" ]; then #verify if dhcp server has been installed
+   installDhcpServer
+   expectGenerate
+   configGenerate
+   changeInterface
+   rm /tmp/DHCP-exp
+else
+echo -p "=========\nisc-dhcp-server installed skip install\n========"
+   expectGenerate
+   configGenerate
+   changeInterface
+fi
